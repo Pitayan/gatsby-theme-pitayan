6cb35fa019daec42324486ef4fa7a38667783aa1
Yanze Dai <ginoalex8964@yahoo.com>
2022-01-07 00:54:43 +0900

    fix: safari smooth scroll polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,14 @@
 // global stylesheet
 import "@/assets/css/style.css"
 
+// Polyfill smooth scroll
+import smoothscroll from "smoothscroll-polyfill"
 import { checkIfDarkMode, toggleDark } from "@/utils"
 
 import "typeface-merriweather"
 
 // toggle Dark mode
 toggleDark(checkIfDarkMode())
+
+// kick off polyfill
+smoothscroll.polyfill()
