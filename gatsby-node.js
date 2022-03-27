a2856ce9e220ad3eabb848ead4e101a2b55bcc98
Yanze Dai <ginoalex8964@yahoo.com>
2022-03-27 11:48:34 +0900

    fix: related posts to use default all posts if no posts related to current post

--- gatsby-node.js
+++ gatsby-node.js
@@ -168,7 +168,7 @@ exports.createResolvers = ({ createResolvers }) => {
       relatedPosts: {
         type: ['Mdx'],
         resolve: async (source, _, context, __) => {
-          const posts = await context.nodeModel.runQuery({
+          let posts = await context.nodeModel.runQuery({
             type: 'Mdx',
             query: {
               // FIXME: runQuery ignores `limit` field in Gatsby V3. Uncommenting this will help resolve the FIXME below in V4
@@ -186,7 +186,20 @@ exports.createResolvers = ({ createResolvers }) => {
             },
           })
 
-          if (!posts || !posts.length) return []
+          if (!posts || !posts.length) {
+            // Get all articles if there's no related posts
+            posts = await context.nodeModel.runQuery({
+              type: 'Mdx',
+              query: {
+                filter: {
+                  id: {
+                    ne: source.id,
+                  },
+                },
+              },
+            })
+          }
+
           // FIXME: temporarily force posts length to 3
           posts.length > 3 && (posts.length = 3)
 
