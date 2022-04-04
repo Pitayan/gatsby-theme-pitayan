cd14eab4a2a3a6d36bee11bb8e7216b46484f2df
Yanze Dai <ginoalex8964@yahoo.com>
2022-04-05 00:44:34 +0900

    style(gatsby-theme-pitayan): prettier

--- packages/gatsby-theme-pitayan/src/components/HomeLinks/index.tsx
+++ packages/gatsby-theme-pitayan/src/components/HomeLinks/index.tsx
@@ -7,7 +7,11 @@ const HomeLinks: React.FC = () => {
 
   return (
     <div className="flex flex-wrap">
-      <NavigationLinksGroup group="home" data={siteLinks} className="mr-6 py-2" />
+      <NavigationLinksGroup
+        group="home"
+        data={siteLinks}
+        className="mr-6 py-2"
+      />
     </div>
   )
 }
