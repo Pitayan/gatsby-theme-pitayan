35243536ca188a729e064e09484ba2329d85e62e
Yanze Dai <ginoalex8964@yahoo.com>
2022-04-06 10:01:14 +0900

    fix(gatsby-theme-pitayan): key prop for the inline js

--- packages/gatsby-theme-pitayan/src/gatsby/node/on-render-body.js
+++ packages/gatsby-theme-pitayan/src/gatsby/node/on-render-body.js
@@ -3,7 +3,7 @@ const React = require("react")
 const HeadComponents = [
   // Refers to https://tailwindcss.com/docs/dark-mode
   // This is to set theme color dark via an inline JS to circumvent a flash of color change during page rendering
-  <script dangerouslySetInnerHTML={{ __html: `
+  <script key="tailwind-toggle-dark-mode" dangerouslySetInnerHTML={{ __html: `
     (function() {
       if (typeof window == 'undefined') return;
       if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
