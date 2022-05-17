c717255495f6fe7f6e2370717371c79de87c84c1
Yanze Dai <ginoalex8964@yahoo.com>
2022-05-17 16:11:35 +0900

    feat(gatsby-theme-pitayan): TopNav to have container class prop

--- packages/gatsby-theme-pitayan/src/components/TopNav/index.tsx
+++ packages/gatsby-theme-pitayan/src/components/TopNav/index.tsx
@@ -4,11 +4,17 @@ import DarkModeToggle from "@pitayan/gatsby-theme-pitayan/src/components/DarkMod
 import SiteLogo from "@pitayan/gatsby-theme-pitayan/src/components/SiteLogo"
 import { useSiteMetadata } from "@pitayan/gatsby-theme-pitayan/src/hooks"
 
-const TopNav: React.FC = () => {
+type TopNavProps = {
+  className?: string
+}
+
+const TopNav: React.FC<TopNavProps> = ({
+  className = "",
+}) => {
   const { name, title, icon } = useSiteMetadata()
 
   return (
-    <nav className="block flex justify-between content-center">
+    <nav className={`block flex justify-between content-center ${className}`}>
       <Link to="/" className="flex space-x-3 items-center">
         <SiteLogo className="h-8 w-8" alt={title} icon={icon} />
         <b className="font-serif font-extrabold tracking-wide text-2xl hidden md:block">
