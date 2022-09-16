72209c05b16559991b1b47fadaf124a27e7dfa29
Yanze Dai <ginoalex8964@yahoo.com>
2022-09-16 21:46:04 +0900

    Updates

---
+++ a981ec11-33492a1d791b4647a6b0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwww=self.webpackChunkwww||[]).push([[400],{313:function(t,r,e){e.d(r,{C3L:function(){return a}});var n=e(6963);function a(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"rect",attr:{x:"9",y:"9",width:"13",height:"13",rx:"2",ry:"2"}},{tag:"path",attr:{d:"M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"}}]})(t)}}}]);
+//# sourceMappingURL=a981ec11-33492a1d791b4647a6b0.js.map
